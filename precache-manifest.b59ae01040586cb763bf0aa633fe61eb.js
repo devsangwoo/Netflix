cc7f09d652bee0d0f305bf980fefd22f385740da
sangwoocho <devsangwoo@gmail.com>
2020-01-14 21:46:43 +0900

    Updates

---
+++ precache-manifest.b59ae01040586cb763bf0aa633fe61eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520455155a1b75c76547e4b92e41876e",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "aee5bfb435e9cda2d10f",
+    "url": "/netflix/static/js/2.de1a7067.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/netflix/static/js/2.de1a7067.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e2799951a0a5472fa31",
+    "url": "/netflix/static/js/main.1cf4ba7d.chunk.js"
+  },
+  {
+    "revision": "740b4bc178c387f7a3ce",
+    "url": "/netflix/static/js/runtime-main.02b6b7ec.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/netflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
