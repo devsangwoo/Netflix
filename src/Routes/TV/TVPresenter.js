92335330edca4103d53aa7f04f176493147a50d0
sangwoo <dev.sangwoo@gmail.com>
2020-01-29 16:02:54 +0900

    update TVpresenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,11 +1,11 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import Helmet from "react-helmet";
-import Section from "../../Components/Section";
-import Loader from "../../Components/Loader";
-import Message from "../../Components/Message";
-import Poster from "../../Components/Poster";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import Helmet from 'react-helmet';
+import Section from '../../Components/Section';
+import Loader from '../../Components/Loader';
+import Message from '../../Components/Message';
+import Poster from '../../Components/Poster';
 
 const Container = styled.div`
   padding: 20px;
@@ -67,3 +67,12 @@ const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => (
     )}
   </>
 );
+TVPresenter.propTypes = {
+  topRated: PropTypes.array,
+  popular: PropTypes.array,
+  airingToday: PropTypes.array,
+  loading: PropTypes.bool.isRequired,
+  error: PropTypes.string,
+};
+
+export default TVPresenter;
