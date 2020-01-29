a5356b970b61a432a781dcd49e92f82764487dc5
sangwoo <dev.sangwoo@gmail.com>
2020-01-29 15:45:08 +0900

    Updates

---
+++ precache-manifest.3d0a9be26e0ac757ec26ec5c556274df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e395c676a5579d42ac57c0c9c08c76e",
+    "url": "/Netflix/index.html"
+  },
+  {
+    "revision": "0eeb2a3507dbdce0df71",
+    "url": "/Netflix/static/js/2.2ff505ba.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/Netflix/static/js/2.2ff505ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a63b7a591a0969ba082",
+    "url": "/Netflix/static/js/main.9d137a5e.chunk.js"
+  },
+  {
+    "revision": "c0a4ffe074ce92b2e36b",
+    "url": "/Netflix/static/js/runtime-main.6dab2767.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/Netflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netflix/precache-manifest.b59ae01040586cb763bf0aa633fe61eb.js"
+  "/Netflix/precache-manifest.3d0a9be26e0ac757ec26ec5c556274df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/netflix/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Netflix/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
